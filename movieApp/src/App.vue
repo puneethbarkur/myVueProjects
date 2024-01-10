e368b77be7691bdf1f57ae847b338b40cc27870f
puneethbarkur <puneethbkr98@gmail.com>
2024-01-10 10:37:41 +0530

    movieApp

---
+++ movieApp/src/App.vue
@@ -0,0 +1,85 @@
+<script setup>
+  import movieData from './data.js';
+  import {ref} from 'vue';
+
+  let text = ref("");
+  let arr = ref([]);
+  function pu(){
+    arr.value = movieData.filter((ele)=>{
+      return ele.name.toLowerCase().indexOf(text.value.toLowerCase()) !== -1;
+    });
+  }
+</script>
+
+<template>
+  <h1>The Marvel Cenematic Universe</h1>
+  <input type="text" placeholder="Search the Movie" v-model="text" @input="pu"/>
+  <ul>
+    <li v-for="i in arr">
+      <div>
+        <img :src="`${i.url}`" :alt="`${i.name}`"/>
+      </div>
+      <div class="details">
+        {{ i.name }} 
+        <div class="year">{{ i.year }}</div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+}
+
+  ul{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+    margin-top: 20px;
+  }
+
+  li{
+    list-style: none;
+    border-radius: 10px;
+    cursor: pointer;
+    width: 180px;
+    box-shadow: 0px 3px 3px 2px rgba(0,0,0,0.5);
+    transition: all 0.2s linear;
+
+    & img{
+      border-radius: 10px 10px 0 0;
+      height: 240px;
+      width: 100%;
+    }
+
+    &:hover{
+      box-shadow: none;
+     border:1px solid gray;
+    }
+  }
+
+  .details{
+    padding-left: 10px; 
+  }
+
+  input{
+    width: 400px;
+    height: 50px;
+    border: 3px solid #892ad2;
+    padding: 5px;
+    outline: none;
+    border-radius: 10px;
+    display: block;
+    margin: 20px auto;
+  }
+  h1{
+    text-align: center;
+  }
+</style>
---
+++ movieApp/src/data.js
@@ -0,0 +1,84 @@
+const movieData = [
+{
+    url: '/src/assets/pics/Iron_Man.jpg',
+    name: "Iron Man",
+    year: 2008
+},
+{
+    url: '/src/assets/pics/The_Incredible_Hulk.jpg',
+    name: "The Incredible Hulk",
+    year: 2008
+},
+{
+    url: '/src/assets/pics/Ant-Man.jpg',
+    name: "Ant man",
+    year: 2015
+},
+{
+    url: '/src/assets/pics/Avengers_Endgame.jpg',
+    name: "Avengers Endgame",
+    year: 2019
+},
+{
+    url: '/src/assets/pics/Avengers_Infinity.jpg',
+    name: "Avengers Infinity War",
+    year: 2018
+},
+{
+    url: '/src/assets/pics/Black_Panther.jpg',
+    name: "Black Panther",
+    year: 2019
+},
+{
+    url: '/src/assets/pics/Captain_America_Civil_War.jpg',
+    name: "Captain America civil war",
+    year: 2016
+},
+{
+    url: '/src/assets/pics/Captain_America_The_First_Avenger.jpg',
+    name: "Captain America The first avenger",
+    year: 2011
+},
+{
+    url: '/src/assets/pics/Captain_Marvel.jpg',
+    name: "Captain Marvel",
+    year: 2018
+},
+{
+    url: '/src/assets/pics/Doctor_Strange.jpg',
+    name: "Doctor Strange",
+    year: 2017
+},
+{
+    url: '/src/assets/pics/Guardians_of_the_Galaxy.jpg',
+    name: "Guardians of the Galaxy",
+    year: 2014
+},
+{
+    url: '/src/assets/pics/Spider-Man.jpg',
+    name: "Spider Man Home Coming",
+    year: 2017
+},
+{
+    url: '/src/assets/pics/The_Avengers.jpg',
+    name: "The Avengers",
+    year: 2012
+},
+{
+    url: '/src/assets/pics/Thor_Ragnarok.jpg',
+    name: "Thor Ragnarok",
+    year: 2018
+},
+{
+    url: '/src/assets/pics/Thor_The_Dark_World.jpg',
+    name: "Thor the dark world",
+    year: 2013
+},
+{
+    url: '/src/assets/pics/Thor.jpg',
+    name: "Thor",
+    year: 2011
+},
+];
+
+export default movieData;
